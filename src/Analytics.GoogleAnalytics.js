ef76e4506c144c138a6a588b167f07413f5582be
Tim Benniks <tim@timbenniks.nl>
2012-09-17 17:11:06 +0200

    Did a small update to the google analytics implementation

--- src/Analytics.GoogleAnalytics.js
+++ src/Analytics.GoogleAnalytics.js
@@ -42,17 +42,9 @@
 			window._gaq.push(['_trackPageview', trackingData.url]);
 		},
 		
-		// @category (required)			The name you supply for the group of objects you want to track.
-		// @action (required)			A string that is uniquely paired with each category, and commonly used to define the type of user interaction for the web object.
-		// @label (optional)			An optional string to provide additional dimensions to the event data.
-		// @value (optional)			An integer that you can use to provide numerical data about the user event.
-		// @non-interaction (optional)	A boolean that when set to true, indicates that the event hit will not be used in bounce-rate calculation.
 		trackEvent = function(e)
 		{
 			var trackingData = mapTrackingData('trackEvent', e.trackingData);
-
-            console.log(trackingData);
-
 			window._gaq.push(['_trackEvent', trackingData.category, trackingData.action, trackingData.opt_label, trackingData.opt_value, trackingData.opt_noninteraction]);
 		},
 
